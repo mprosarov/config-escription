e2880b228ebfa37cec3090d59c5b33211cbf36a0
MaxD <controldms@gmail.com>
2025-02-09 21:53:10 +0300

    Актуализировал описание, добавил сам проект с примером

---
+++ example/JSON/pageLayouts/pageLayout_checklist.js
@@ -0,0 +1,82 @@
+const pageLayout = {
+  navpanel: [
+    {
+      name: "Чек-лист",
+      type: "title",
+      elemtype: "title",
+    },
+    {
+      id: "sidebarID",
+      type: "sidebar",
+      elemtype: "sidebar",
+      icon: "caret-left.svg",
+      position: "top",
+    },
+    {
+      name: "",
+      icon: "svgPrint",
+      class: "secondary",
+      elemtype: "button",
+      type: "dropdown",
+      multiselect: false,
+      multilevel: false,
+      status: "unable",
+      items: [
+        {
+          name: "значение_1",
+          value: "",
+          type: "link",
+          action: {
+            params: [],
+            func: [],
+            url: "",
+          },
+        },
+        {
+          name: "значение_2",
+          value: "",
+          type: "link",
+          action: {
+            params: [],
+            func: [],
+            url: "",
+          },
+        },
+      ],
+    },
+    {
+      name: "",
+      type: "button-group",
+      elemtype: "button-group",
+      items: [
+        {
+          name: "",
+          class: "secondary",
+          icon: "svgSave",
+          action: {
+            params: [],
+            func: [],
+          },
+        },
+        {
+          name: "",
+          icon: "svgRefresh",
+          class: "secondary",
+          action: {
+            params: [],
+            func: [],
+          },
+        },
+        {
+          name: "",
+          class: "secondary",
+          icon: "svgCancel",
+          action: {
+            params: [],
+            func: [],
+          },
+        },
+      ],
+    },
+  ],
+};
---
+++ example/js/tableModule.js
@@ -0,0 +1,31 @@
+const CreateTable = (function (){
+
+    function create(parent, config) {
+        contentTable = `<h5>${config["name"]}</h5>
+                            <div id="${config["id"]}"></div>`;
+        parent.insertAdjacentHTML("beforeend", contentTable);
+
+        recursiveSearchColumns(config["tdata"], "columns");
+        if (config["indexCols"]) {
+            config["tdata"]["data"].unshift(config["indexCols"]);
+            config["tdata"]["frozenRows"] = 1;
+        }
+        var table = new Tabulator(`#${config["id"]}`, config["tdata"]);
+    }
+    function recursiveSearchColumns(data, target) {
+      let values = {
+        headerHozAlign: "center",
+        hozAlign: "center",
+        headerWordWrap: true,
+      };
+      if (!data[target] || data[target].length < 1) return;
+      for (let i = 0; i < data[target].length; i++) {
+        let child = data[target][i];
+        child = Object.assign(child, values);
+        recursiveSearchColumns(child, target);
+      }
+    }
+    return {
+      create,
+    };
+})();
