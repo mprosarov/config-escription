fa0b1875b6bfc144693faf4cec8de0b1040aae59
CrimsonHedgehog <an.ka.system@mail.ru>
2025-05-14 14:25:56 +0300

    url

--- example/dist/js/app.js
+++ example/dist/js/app.js
@@ -171,7 +171,7 @@ class BaseElement {
   }
 }
 // У конечного пункта могут быть варианты действий (переход на страницу или что-то другое)
-// свойство "action": "redirect" - загрузка новой страницы, по имени конфигурации json, указанной в свойстве "link"
+// свойство "action": "redirect" - загрузка новой страницы, по имени конфигурации json, указанной в свойстве "config"
 class Menu {
   static TYPE = "menu";
   constructor(parent, menuData) {
@@ -191,7 +191,7 @@ class Menu {
   }
 
   createMenuItem(parentEl, item) {
-    parentEl.insertAdjacentHTML("beforeend", `<li class="nav-item"><a class="nav-link" href="${item.link}">${item.title}</a></li>`);
+    parentEl.insertAdjacentHTML("beforeend", `<li class="nav-item"><a class="nav-link" href="${item.config}">${item.title}</a></li>`);
     const li_element = parentEl.lastElementChild;
     const a_element = li_element.lastElementChild;
     if (!item.submenu || item.submenu.length == 0) return li_element;
@@ -213,7 +213,7 @@ class Menu {
     parentEl.insertAdjacentHTML(
       "beforeend",
       `<li>
-        <a class="dropdown-item" href="${item["link"] || "#"}">${item.title}</a>
+        <a class="dropdown-item" href="${item["config"] || "#"}">${item.title}</a>
       </li>`
     );
     const li_element = parentEl.lastElementChild;
@@ -221,35 +221,39 @@ class Menu {
     // Если нет подменю - возвращаем элемент
     if (!item.submenu || item.submenu.length == 0){
       //TODO: проверять какое действие нужно делать если нет подменю
-      // если есть свойство и  action равно redirect, то загружаем страницу по имени из свойства link
+      // если есть свойство и  action равно redirect, то загружаем страницу по имени из свойства config
       //if(!item['action'] || item['action'] == 'redirect'){
       if(!item['action']||item['action']==''){
         a_element.onclick = () => {
           event.preventDefault();
-          PageBuilder.loadPageConfig(item.link);
+          PageBuilder.loadPageConfig(item.config);
         };
       }
-      
+      var url = window.location.href;
+
       if(item['action'] == 'redirect'){
         a_element.onclick = () => {
           event.preventDefault();
-          if(item['action']['link']){
-            let url = window.location.href;
-            console.log(url)
-            let searchParams = new URLSearchParams(url);
-            let urlConfigName = searchParams.get("config");
-            if(urlConfigName === null){
-              searchParams.append("config",item['link'])
-           //  window.open(url+''+ searchParams.toString(), '_blank').focus();
-           //  PageBuilder.loadPageConfig(item.link);
-           
-            console.log(url+'?'+ encodeURIComponent(searchParams.toString()))
-            //console.log(urlConfigName)
-            } ;
+
+          if(item['url']){
+            if(item['url']==""){
+              throw new Error(`не указан url`);
+            }else window.open(item['url'], '_blank').focus();
+            return
           }
+          let searchParams = new URLSearchParams(url);
+          let urlConfigName = searchParams.get("config");
+          url = url+'?config='+ encodeURIComponent(item['config'])
+          if(!item['newtab']||item['newtab'] == false)
+            PageBuilder.loadPageConfig(item.config);
+          else{
+            if(urlConfigName === null){
+              searchParams.append("config",item['config'])
+              window.open(url, '_blank').focus();
+            };
+          }    
         } 
       }
-//window.open(url, '_blank').focus();
       return li_element;
     }
     // У пункта есть подменю - добавляем необходимые классы
